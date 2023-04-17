bbf4863a68a8f64caadee115d0dc759f2d70ff52
joaov <joaovitorcgds@gmail.com>
2023-04-16 22:11:58 -0300

    ingles to portugues

--- components/Footer.js
+++ components/Footer.js
@@ -3,7 +3,7 @@ import styles from "../styles/Footer.module.css";
 export default function Footer(){
 	return(
 		<footer className={styles.container}>
-			<span>Created by João Vitor Costa in 2023.</span>        
+			<span>Criado por João Vitor Costa em 2023.</span>        
 		</footer>
 	);
 }
